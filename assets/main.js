07a2dfa1c4b99336cfd2e26744f13b696002e0ba
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-01-22 15:08:01 -0500

    new

---
+++ assets/main.js
@@ -0,0 +1,109 @@
+/* global moment firebase */
+
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBRz_C7pIldj2ZAb6479BpsX8ee9c33mj8",
+        authDomain: "sample-db-a0396.firebaseapp.com",
+        databaseURL: "https://sample-db-a0396.firebaseio.com",
+        projectId: "sample-db-a0396",
+        storageBucket: "sample-db-a0396.appspot.com",
+        messagingSenderId: "960877017269"
+      };
+      firebase.initializeApp(config);
+    
+    // Create a variable to reference the database
+    var database = firebase.database();
+    
+    $("#add-employee-btn").on("click", function(event) {
+      event.preventDefault();
+     
+      // Grabs user input
+      var empName = $("#employee-name-input").val().trim();
+      var empRole = $("#role-input").val().trim();
+      var empStart = $("#start-input").val().trim();
+      var empRate = $("#rate-input").val().trim();
+     
+      // Creates local "temporary" object for holding employee data
+      var newEmp = {
+        name: empName,
+        role: empRole,
+        start: empStart,
+        rate: empRate
+      };
+     
+      // Uploads employee data to the database
+      console.log(newEmp);
+      database.ref().push(newEmp);
+     
+      // Logs everything to console
+      console.log(newEmp.name);
+      console.log(newEmp.role);
+      console.log(newEmp.start);
+      console.log(newEmp.rate);
+     
+      // Alert
+      alert("Employee successfully added");
+     
+      // Clears all of the text-boxes
+      $("#employee-name-input").val("");
+      $("#role-input").val("");
+      $("#start-input").val("");
+      $("#rate-input").val("");
+     });
+     
+     // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+     database.ref().on("child_added", function(employeeSnapshot) {
+     
+      console.log(employeeSnapshot.val());
+     
+      // Store everything into a variable.
+      var empName = employeeSnapshot.val().name;
+      var empRole = employeeSnapshot.val().role;
+      var empStart = employeeSnapshot.val().start;
+      var empRate = employeeSnapshot.val().rate;
+     
+      // Employee Info
+      console.log(empName);
+      console.log(empRole);
+      console.log(empStart);
+      console.log(empRate);
+     
+      // Prettify the employee start
+      var empStartPretty = moment(empStart).format("MM/DD/YY");
+      var empStartMoment = moment(empStart, "YYYY-MM-DD");
+     
+      // Calculate the months worked using hardcore math
+      // To calculate the months worked
+      var empMonths = Math.abs(empStartMoment.diff(moment(), "months"));
+      console.log(empMonths);
+     
+      // Calculate the total billed rate
+      var empBilled = empMonths * empRate;
+      console.log(empBilled);
+    
+      var createRow = function(data) {
+        // Create a new table row element
+        var tRow = $("<tr>");
+    
+        // Methods run on jQuery selectors return the selector they we run on
+        // This is why we can create and save a reference to a td in the same statement we update its text
+        var emName = $("<td>").text(empName);
+        var emRole = $("<td>").text(empRole);
+        var emStartDate = $("<td>").text(empStartPretty);
+        var emMonths = $("<td>").text(empMonths);
+        var emMonthly = $("<td>").text(empRate);
+        var totalBilled = $("<td>").text(empBilled);
+    
+        // Append the newly created table data to the table row
+        tRow.append(emName, emRole, emStartDate, emMonths, emMonthly, totalBilled);
+        // Append the table row to the table body
+        $("#currentEmployees").append(tRow);
+        
+      };
+    
+      createRow();
+      
+     });
+    });
+    
