58fc8fb308b0624da29db75f1f4ff03ef1ff1e05
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-01-24 16:34:16 -0500

    new

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Train-Scheduler
 
 updated portfolio: https://ss100yan.github.io/Portfolio-BS/
+
+It is train schedule application using the realtime database on Firebase.
+The data is retrieved from Firebase and manipulated with Moment.js.
--- assets/main.js
+++ assets/main.js
@@ -88,4 +88,15 @@ $(document).ready(function () {
                         createRow();
                       });
                    });
+
+                function display_c(){
+              var refresh=1000; // Refresh rate in milli seconds
+            mytime=setTimeout('display_ct()',refresh)
+          }
+       function display_ct() {
+     var x = new Date()
+   document.getElementById('ct').innerHTML = x;
+display_c();
+}
+
     
